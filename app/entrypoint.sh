8f903ec30e2fd6a5acc6b37ce10337429e63cdd2
zai <artziga77@gmail.com>
2023-12-18 22:44:38 +0300

    nginx added

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -14,7 +14,7 @@ fi
 cd app
 
 poetry run python manage.py migrate --noinput
-#poetry run python manage.py collectstatic --noinput
+poetry run python manage.py collectstatic --noinput
 #poetry run gunicorn config.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
