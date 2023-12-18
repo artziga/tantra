1f435663d99dae7deb8625fe4a10eed2e4f7cffd
zai <artziga77@gmail.com>
2023-12-18 21:37:05 +0300

    docker compose modified

--- app/config/settings.py
+++ app/config/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("DB_USER", "user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 cd app
+
 poetry run python manage.py migrate --noinput
 #poetry run python manage.py collectstatic --noinput
 #poetry run gunicorn config.wsgi:application --bind 0.0.0.0:8000
-poetry run python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
