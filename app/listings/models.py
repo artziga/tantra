27b9bd147e63b6cf9bf25d911d120e0b77327e42
zai <artziga77@gmail.com>
2024-01-06 21:03:24 +0300

    feature: listing admin names

--- app/listings/models.py
+++ app/listings/models.py
@@ -27,5 +27,9 @@ class Feature(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     icon = models.CharField(max_length=50, verbose_name='Иконка', null=True)
 
+    class Meta:
+        verbose_name = 'удобство'
+        verbose_name_plural = 'удобства'
+
     def __str__(self):
         return self.name
