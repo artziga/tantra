b837035929494f484ad91a43d0dad0a27c1e15e7
zai <artziga77@gmail.com>
2024-01-08 14:42:31 +0300

    fix: years suffix fixed

--- app/users/templatetags/format_years.py
+++ app/users/templatetags/format_years.py
@@ -13,7 +13,7 @@ def years_suffix(value):
         return
     if value % 10 == 1 and value % 100 != 11:
         suffix = "год"
-    elif value % 10 in [0, 2, 3, 4] and value % 100 not in [12, 13, 14]:  # тут 0 потому что указывается <1 года
+    elif value == 0 or value % 10 in [2, 3, 4] and value % 100 not in [12, 13, 14]:  # тут 0 потому что указывается <1 года
         suffix = "года"
     else:
         suffix = "лет"
