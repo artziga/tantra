0157476689e731d663beda082cdda9aca67b160b
zai <artziga77@gmail.com>
2024-01-27 21:56:04 +0300

    fix: image size notification added

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from gallery.forms import max_image_size_text
+
 User = get_user_model()
 
 
 class EditProfileForm(forms.ModelForm):
 
-    avatar = forms.ImageField(label='Аватар', required=False, widget=forms.ClearableFileInput())
+    avatar = forms.ImageField(label='Аватар', required=False, help_text=max_image_size_text,
+                              widget=forms.ClearableFileInput())
     first_name = forms.CharField(label='Имя', required=False, widget=forms.TextInput(
         attrs={'class': 'form__input'}))
     last_name = forms.CharField(label='Фамилия', required=False, widget=forms.TextInput(
