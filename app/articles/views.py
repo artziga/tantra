b2365440209022aac08024849a9ae897e813589d
zai <artziga77@gmail.com>
2024-01-31 21:38:05 +0300

    doc: articles app documentated

--- app/articles/views.py
+++ app/articles/views.py
@@ -4,18 +4,33 @@ from articles.models import Article, Announcement
 
 
 class ArticleView(DetailView):
+    """
+    Класс представления для отображения деталей статьи.
+
+    """
+
     model = Article
     context_object_name = 'article'
     extra_context = {'title': 'Статья'}
 
 
 class AnnouncementView(DetailView):
+    """
+    Класс представления для отображения деталей анонса.
+
+    """
+
     model = Announcement
     context_object_name = 'announcement'
     extra_context = {'title': 'Анонс'}
 
 
 class ArticleListView(ListView):
+    """
+    Класс представления для отображения списка статей.
+
+    """
+
     model = Article
     paginate_by = 10
     extra_context = {'title': 'Статьи'}
@@ -23,6 +38,11 @@ class ArticleListView(ListView):
 
 
 class AnnouncementListView(ListView):
+    """
+    Класс представления для отображения списка анонсов.
+
+    """
+
     model = Announcement
     paginate_by = 10
     extra_context = {'title': 'Анонсы'}
